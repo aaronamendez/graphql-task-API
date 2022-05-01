ff6d0e8028c65adecd4db73e452ab140c9d0b680
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-05-01 17:38:39 -0400

    Adding GraphQL.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello from typescript');
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (_, res: Response): void => {
+	res.json({ message: 'Typescript' });
+});
+
+app.listen(3000, (): void => console.log('Server Running ...'));
